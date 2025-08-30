075819d0ed709f3ea9fdc783070cc8ece89e7625
Git-teja547 <saitejagaini108@gmail.com>
2025-08-30 19:25:21 +0530

    first commit

--- HelloUser.java
+++ HelloUser.java
@@ -2,14 +2,9 @@ import java.util.Scanner;
 
 public class HelloUser {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        
 
         System.out.println("Hello, World!");
-        System.out.print("What's your name? ");
-
-        String name = scanner.nextLine();
-        System.out.println("Nice to meet you, " + name + "!");
-
-        scanner.close();
+       
     }
 }
